ffb82b2e408aba882d9423b5fad73f0d519e4c31
Akbar Azam <akbarazam754@gmail.com>
2026-08-27 12:40:13 +0600

    Updated Python project

--- calculator.py
+++ calculator.py
@@ -1,3 +1,32 @@
+# num1 = int(input("Enter 1st number : "))
+# num2 = int(input("Enter 2nd number : "))
+
+# operator = input("Enter a an operator : ")
+
+# if operator == "+":
+#     print(f"the addition of {num1} and {num2} is = ", num1 + num2)
+
+# elif operator == "-":
+#     print(f"the subtraction of {num1} and {num2} is = ", num1 - num2)
+    
+# elif operator == "*":
+#     print(f"the multiplication of {num1} and {num2} is = ", num1 * num2)
+
+# elif operator == "/":
+#     print(f"the division of {num1} and {num2} is = ", num1 / num2)
+    
+# elif operator == "//":
+#     print(f"the floor division of {num1} and {num2} is = ", num1 // num2)
+    
+# elif operator == "%":
+#     print(f"the reminder for {num1} and {num2} is = ", num1 % num2)
+    
+# else:
+#     print("SORRY! YOU HAVE ENTERED AN INCORRECT OPERATOR")    
+
+      
+
+
 
     
 
