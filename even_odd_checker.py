c6a9d753d7684ecefba2328fb759bdb0aa1a37d0
Akbar Azam <akbarazam754@gmail.com>
2026-08-27 13:24:16 +0600

    updated

---
+++ even_odd_checker.py
@@ -0,0 +1,10 @@
+num = int(input("Enter a number : "))
+
+if num % 2 == 0:
+    print(f"the number {num} is an EVEN")
+
+elif num % 2 != 0:
+    print(f"the number {num} is an ODD")
+
+else:
+    print("INVALID NUM")
